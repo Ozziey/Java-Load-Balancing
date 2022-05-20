5a5953dd7f98c679fe8f7497e4352e604368e0d8
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-20 17:54:01 +0300

    added server

--- LoadBalancing/src/UDPServer.java
+++ LoadBalancing/src/UDPServer.java
@@ -2,8 +2,7 @@ import java.net.*;
 
 class UDPServer {
 	public static void main(String args[]) throws Exception 
-	{ 
-
+	{
 		DatagramSocket serverSocket = new DatagramSocket(9876); 
 
 		byte[] receiveData = new byte[1024]; 
---
+++ LoadBalancing/src/client.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.net.*;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
